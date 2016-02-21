62e62042417f8909d6d0949eca1d8937bc833535
Richard Lincoln <r.w.lincoln@gmail.com>
2016-02-21 10:06:45 +0100

    Adding Dialog class.

--- lib/dialog_polyfill.dart
+++ lib/dialog_polyfill.dart
@@ -3,11 +3,35 @@ library dialog_polyfill;
 import 'dart:html';
 import 'dart:js';
 
-registerDialog(Element dialog) {
-  context['dialogPolyfill']?.callMethod('registerDialog', dialog);
-}
+class Dialog {
+  final Element dialog;
+  JsObject _proxy;
 
-/// Show the [dialog] modally.
-showModal(Element dialog) {
-  new JsObject.fromBrowserObject(dialog).callMethod('showModal');
+  Dialog(this.dialog) {
+    _proxy = new JsObject.fromBrowserObject(dialog);
+    if (!_proxy.hasProperty('showModel')) {
+      context['dialogPolyfill']?.callMethod('registerDialog', [dialog]);
+    }
+  }
+
+  /// Show the [dialog] modally.
+  showModal() {
+    _proxy.callMethod('showModal');
+  }
+
+  /// Open or close this dialog.
+  void set open(bool open) {
+    _proxy.callMethod('setOpen', [open]);
+  }
+
+  /// Shows the dialog. This is idempotent and will always succeed.
+  void show() {
+    _proxy.callMethod('show');
+  }
+
+  /// Closes the dialog. This is optional vs clearing the open
+  /// attribute, however this fires a `close` event.
+  void close() {
+    _proxy.callMethod('close');
+  }
 }
